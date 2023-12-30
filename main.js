37006e8c9acc4fa90f4a8849da78b11fd1dc5b53
Stradex <stradexengine@gmail.com>
2023-12-30 19:22:20 -0300

    FIX: Remove old code from main.js

--- main.js
+++ main.js
@@ -1,31 +1,3 @@
-
-function startSnake() {
-    printToConsole("Snake game started...");
-    getConsoleHTMLElement().style.lineHeight = 1;
-    getConsoleHTMLElement().style.letterSpacing = "0.65em";
-    updateGraphics(35);
-}
-
-function updateGraphics(fps){
-    clearConsole();
-    drawGraphics(20,15);
-
-    setTimeout(function() { updateGraphics(fps) }, parseInt(1000.0/fps)); //ms
-}
-
-function drawGraphics(width,height){
-    let graphicString="";
-    for (let j=0; j<height ; j++){
-        for (let i=0; i<width ; i++){
-            graphicString+="#";
-        }
-        graphicString+="\n";
-    }
-    
-    printToConsole(graphicString);
-
-}
-
 async function main() {
     //Acá escribimos nuestro programa.
     printToConsole("::Game Master 7500SX::");
